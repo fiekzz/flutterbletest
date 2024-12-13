11e2d11801b898de2a343366b3f3da2e9279a380
fiekzz <fikrichuck@gmail.com>
2024-12-14 04:21:48 +0800

    ✨ Refactor Teltonika class to include branch information

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,13 +1,18 @@
 class Constants {
   static List<Teltonika> teltonikas = [
-    Teltonika('7CD9F4013359', 'MG1_A010AA'),
-    Teltonika(null, 'MG1_3FFBAA'),
+    Teltonika('7C:D9:F4:04:87:C6', 'MG1_A010AA', 'Taman Equine'),
+    Teltonika('7C:D9:F4:01:33:59', 'MG1_3FFBAA', 'Cyberjaya'),
   ];
 }
 
 class Teltonika {
-  String? mac;
+  String mac;
   String advName;
+  String branch;
 
-  Teltonika(this.mac, this.advName);
+  Teltonika(
+    this.mac,
+    this.advName,
+    this.branch,
+  );
 }
